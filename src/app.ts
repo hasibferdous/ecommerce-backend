28ecc56de2b091048634d0c5f23f5d52bdf78870
hasibferdous <hasibferdous.ict@gmail.com>
2024-07-04 11:48:42 +0600

    eslint configuration done

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+// app.use('/api/v1/products', ProductsRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+};
+
+app.get('/', getAController);
+
+export default app;
